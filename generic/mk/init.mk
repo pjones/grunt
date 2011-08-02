7e3821f41bde623d6eddbbbc844d42a69f80ac60
Peter Jones <pjones@pmade.com>
2011-08-01 18:59:48 -0600

    Small fixes for calling grunt from a subdir

--- generic/mk/init.mk
+++ generic/mk/init.mk
@@ -4,5 +4,8 @@
 .PHONY: install
 
 ################################################################################
+$(if $(GRUNT_OS),,$(eval include $(GRUNT_HOME)/generic/mk/os.mk))
+
+################################################################################
 include $(GRUNT_HOME)/generic/mk/dirs.mk
 include $(GRUNT_HOME)/generic/mk/files.mk
