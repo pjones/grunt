02ea20ea3fcd2a0b7d4eb65d53dc05a372478009
Peter Jones <pjones@pmade.com>
2012-04-30 11:56:40 -0600

    Fix Debian codename detection

--- debian/mk/codename.mk
+++ debian/mk/codename.mk
@@ -6,6 +6,10 @@ else ifeq ($(GRUNT_DEBIAN_VERSION),6.0)
   GRUNT_DEBIAN_CODENAME = squeeze
 else ifeq ($(GRUNT_DEBIAN_VERSION),7.0)
   GRUNT_DEBIAN_CODENAME = wheezy
+else ifeq ($(GRUNT_DEBIAN_VERSION),wheezy/sid)
+  GRUNT_DEBIAN_CODENAME = wheezy
+else ifeq ($(GRUNT_DEBIAN_VERSION),sid)
+  GRUNT_DEBIAN_CODENAME = sid
 else
   GRUNT_DEBIAN_CODENAME = debian-unknown
 endif
