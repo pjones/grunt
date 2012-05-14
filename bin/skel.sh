b3e45576ef300bd51ccf6c1d07cfaded4312ee4b
Peter Jones <pjones@pmade.com>
2012-05-14 10:49:28 -0600

    Make second argument to skel.sh optional

--- bin/skel.sh
+++ bin/skel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
-  echo "Usage: dir-name os-type"
+if [ $# -lt 1 -o $# -gt 2 ]; then
+  echo "Usage: directory-name [operating system]"
   exit 1
 fi
 
@@ -15,17 +15,28 @@ fi
 
 GRUNT=`dirname $0`/..
 
-if [ ! -d $GRUNT/$OS ]; then
-  echo "ERROR: Grunt doesn't support $OS"
-  exit 1
+if [ x$OS = x ]; then
+  OS=`sh $GRUNT/bin/detectos.sh`
+
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Grunt doesn't support this operating system"
+    exit 1
+  fi
+else
+  if [ ! -d $GRUNT/$OS ]; then
+    echo "ERROR: Grunt doesn't support $OS"
+    exit 1
+  fi
 fi
 
 mkdir $DIR
 cp -r $GRUNT/generic/skel/* $DIR/
 [ -d $GRUNT/$OS/skel ] && cp -r $GRUNT/$OS/skel/* $DIR/
 
+URL=`(cd $GRUNT && git config remote.origin.url)`
+
 (cd $DIR
  git init
- git submodule add git://pmade.com/grunt grunt
+ git submodule add $URL grunt
  git add .
  git commit -m "Initial server configuration")
